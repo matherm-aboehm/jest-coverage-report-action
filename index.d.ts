f3ac8c3698b1fab76f3974fae4326cd4e1fafdc8
Andreas Böhm <60391995+matherm-aboehm@users.noreply.github.com>
2026-07-07 13:52:25 +0200

    Revert "Fix module resolution errors for ESM-only package dependencies"
    This reverts commit 108bd8b38ebc7431063cdcb14270e0b53322b175.

--- index.d.ts
+++ index.d.ts
@@ -3,11 +3,3 @@ declare module '*.md' {
 
     export default text;
 }
-
-declare module 'jiti' {
-    export * from 'jiti/static';
-}
-
-declare module 'ohash/utils' {
-    export * from 'c12/node_modules/ohash';
-}
